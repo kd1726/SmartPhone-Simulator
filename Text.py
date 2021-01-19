cc95493655f2ab811b52bef6b0ab22a8e97ed95b
Kevin Doku <35711038+kd1726@users.noreply.github.com>
2021-01-19 18:57:52 -0500

    Update Text.py

--- Text.py
+++ Text.py
@@ -7,14 +7,14 @@
 from twilio.rest import Client
 account_sid ='ACc52255411661040dab50f0f944e722e5'
 auth_token = '0b02b297eca7f0d64baa30f6993c1530'
+#auth_token has been changed for security purposes
 client = Client(account_sid, auth_token)
 who = input("Enter the number that you would like to text: ")
+msg = input('Type your message')
 message = client.messages.create(
     to = who, 
     from_="+12188750645",
-    body = '''Hey babe, How you doin.
-    Im tryna see wassup. You dont check your dms
-    no more?''')
+    body = msg)
 
 print(message.sid)
 
