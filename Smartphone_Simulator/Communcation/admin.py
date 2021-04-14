3e75e4ae5677c93d2ff789c5d45362f540d8d253
kd1726 <kd1726@nyu.edu>
2021-04-13 22:33:52 -0400

    remodeling entire smartphone project

---
+++ Smartphone_Simulator/Communcation/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Call, Text
+# Register your models here.
+admin.site.register(Call)
+admin.site.register(Text)
---
+++ Smartphone_Simulator/Communcation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CommuncationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Communcation'
