6c1ebb73f030f7ffbaeb793cb77563d1a0771f39
kd1726 <kd1726@nyu.edu>
2021-04-19 21:32:13 -0400

    started everthing else

--- Smartphone_Sim/Games/views.py
+++ Smartphone_Sim/Games/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def game_center(request):
     return render(request,"Games/game_center.html")
+
+def blackjack_ask(request):
+    pass
+
+def play_blackjack(request):
+    pass
+
+def guess_the_number(request):
+    pass
--- Smartphone_Sim/Miscellaneous/views.py
+++ Smartphone_Sim/Miscellaneous/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def tool_view(request):
+    pass
+
+def profile(request):
+    pass
+
+def Translate(request):
+    pass
+    
