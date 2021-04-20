0ee1ba15c10db6beef332a93964249c8a3670822
kd1726 <kd1726@nyu.edu>
2021-04-20 11:17:43 -0400

    One game completed

--- Smartphone_Sim/Games/urls.py
+++ Smartphone_Sim/Games/urls.py
@@ -3,5 +3,8 @@ from . import views
 app_name="Games"
 
 urlpatterns = [
-    path("",views.game_center,name="game-center")
+    path("",views.game_center,name="game-center"),
+    path("Black-Jack-Mode/",views.blackjack_ask,name="black-jack-ask"),
+    path("Black-Jack-Single/",views.play_blackjack_single,name="black-jack-single"),
+    path("Black-Jack-Double/",views.play_blackjack_double,name="black-jack-double")
 ]
--- Smartphone_Sim/Games/views.py
+++ Smartphone_Sim/Games/views.py
@@ -5,10 +5,13 @@ def game_center(request):
     return render(request,"Games/game_center.html")
 
 def blackjack_ask(request):
-    pass
+    return render(request,"Games/bjask.html")
 
-def play_blackjack(request):
-    pass
+def play_blackjack_single(request):
+    return render(request,"Games/bjsingle.html")
+
+def play_blackjack_double(request):
+    return render(request,"Games/bjdouble.html")
 
 def guess_the_number(request):
     pass
