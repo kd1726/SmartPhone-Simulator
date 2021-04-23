af211cf76ec5425eaf6e33feeaad01e4499f91d3
kd1726 <kd1726@nyu.edu>
2021-04-22 20:48:53 -0400

    linux likes to play games

--- Communication/views.py
+++ Communication/views.py
@@ -298,6 +298,13 @@ def send_email(request):
         form = CreateEmail(request.POST)
         if form.is_valid():
             if request.POST['username']==request.user.username:
+                server = smtplib.SMTP('smtp.gmail.com:587')
+                server.ehlo()
+                server.starttls()
+                server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+                message = "Subject: {}\n\n".format(request.POST['subject'],request.POST['message']
+                server.send(settings.EMAIL_HOST_USER,receiver_email,message)
+                server.quit()
                 #send_mail(
                 #request.POST['subject'],
                 #request.POST['message'],
